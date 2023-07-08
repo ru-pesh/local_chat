7b192f56f03abcfde6c101c2f47af7339c104e60
rupesh <129289866+ru-pesh@users.noreply.github.com>
2023-07-08 21:10:01 +0545

    Update bla.py

--- bla.py
+++ bla.py
@@ -1,7 +1,7 @@
 import threading
 import os
 #four functions are created for better performance and understandings.
-#below function (wr1) is used to write in a fi
+#below function (wr1) is used to write in a file.
 def wr1():
     global a
     global chat
